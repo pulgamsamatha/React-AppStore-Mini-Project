a9387711a3743e16940c23be57bb1108cf1d75a8
Samatha Pulgam <pulgamsamatha@gmail.com>
2025-10-23 14:36:22 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import AppStore from './Components/AppStore'
+import './App.css';
+
+const App =()=>{
+  return (
+    <>
+      <AppStore/>
+    </>
+  )
+}
+
+export default App
